04f23b56ac458feedb21842dff394b9206dba27a
Saad Mazhar <saad.maz2000@gmail.com>
2021-02-06 16:48:34 -0500

    first commit

---
+++ src/app/SongLib.java
@@ -0,0 +1,17 @@
+package app;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class SongLib extends Application {
+	
+	public void start(Stage primaryStage) {
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		launch(args);
+	}
+
+}
