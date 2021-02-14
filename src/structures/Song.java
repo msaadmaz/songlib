5c83c5b150f53ba01165accf1eb6013d086b912d
KEstabillo <kevinmestabillo@gmail.com>
2021-02-13 22:57:46 -0500

    gui can show with songlist

--- src/structures/Song.java
+++ src/structures/Song.java
@@ -7,20 +7,14 @@ public class Song implements Comparable<Song>{
 	int year;
 
 	public String toString() {
-		return name + " " + artist + " " + album + " " + year;
+		return name;
 	}
 
 	@Override
 	public int compareTo(Song o) {
 		// TODO Auto-generated method stub
 		if( this.name.toLowerCase().compareTo( o.name.toLowerCase() ) == 0 ) {
-//			if( this.artist.toLowerCase().compareTo( o.artist.toLowerCase() ) == 1) {
-//				return 1;
-//			} else if(this.artist.toLowerCase().compareTo( o.artist.toLowerCase() ) == -1) {
-//				return -1;
-//			} else {
-//				return 0;
-//			}
+
 			return this.artist.toLowerCase().compareTo( o.artist.toLowerCase() );
 //			return o.name.toLowerCase().compareTo( this.name.toLowerCase() );
 		}
