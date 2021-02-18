f99cca9ebf77d97a9bcc49bd6fa34a4465d10937
KEstabillo <kevinmestabillo@gmail.com>
2021-02-17 19:04:15 -0500

    made buttons actual functions instead of lambda expressions stuff :)

--- src/view/ListController.java
+++ src/view/ListController.java
@@ -50,20 +50,19 @@ public class ListController {
 	@FXML         
 	TextField updateYear; 
 	private ObservableList<String> obsList;              
-	
-	
+	public static SongList list;
 	
 	public void start(Stage mainStage) {                
 		// create an ObservableList 
 		// from an ArrayList  
 		try( Scanner sc = new Scanner(new File("songs.txt"))){
-			SongList list = new SongList(sc);
+			list = new SongList(sc);
 			obsList = FXCollections.observableArrayList(list.names);
 			songDisplay.setItems(obsList); 
 			
 			// select the first item
 			songDisplay.getSelectionModel().select(0);
-			setTextDisplay(mainStage, list);
+			setTextDisplay(mainStage);
 			
 			// set listener for the items
 			songDisplay
@@ -71,35 +70,7 @@ public class ListController {
 				.selectedIndexProperty()
 				.addListener(
 						(obs, oldVal, newVal) -> 
-						setTextDisplay(mainStage, list));
-			//insert
-			insert.setOnAction((event) -> {
-				String songName = Name.getText();
-				String artistName = Artist.getText();
-				String albumName = Album.getText();
-				int year = Integer.parseInt(Year.getText());
-				insertName(songName, artistName, albumName, year, list);
-				obsList.add(songName);
-				});
-			
-			//delete
-			delete.setOnAction((event) -> {
-				int index = songDisplay.getSelectionModel().getSelectedIndex();
-				obsList.remove(index);
-				list.remove(index);
-				});
-			
-			//Update
-			update.setOnAction((event) -> {
-				int index = songDisplay.getSelectionModel().getSelectedIndex();
-				obsList.remove(index);
-				String songName = updateName.getText();
-				String artistName = updateArtist.getText();
-				String albumName = updateAlbum.getText();
-				int year = Integer.parseInt(updateYear.getText());
-				list.update(index, songName, artistName, albumName, year);
-				obsList.add(songName); //something's up with update function
-				});
+						setTextDisplay(mainStage));
 		}
 		catch (FileNotFoundException e) {
 			System.out.print("file not found");
@@ -107,7 +78,7 @@ public class ListController {
 		}
 		
 	}
-	private void setTextDisplay(Stage mainStage, SongList list) {
+	private void setTextDisplay(Stage mainStage) {
 		int index = songDisplay.getSelectionModel().getSelectedIndex();
 		Song selectedSong = list.list.get(index);
 		display.setText("Name: " + selectedSong.name + " \n " +
@@ -116,8 +87,30 @@ public class ListController {
 						"Year: " + selectedSong.year);
 	}
 	
-	private void insertName(String name, String artist, String album, int year, SongList list) {
-		list.add(name, artist, album, year);
+	public void insertion(ActionEvent e) {
+		String songName = Name.getText();
+		String artistName = Artist.getText();
+		String albumName = Album.getText();
+		int year = Integer.parseInt(Year.getText());
+		list.add(songName, artistName, albumName, year);
+		obsList.add(songName);
+	}
+	
+	public void deletion(ActionEvent e) {
+		int index = songDisplay.getSelectionModel().getSelectedIndex();
+		obsList.remove(index);
+		list.remove(index);
+	}
+	
+	public void update(ActionEvent e) {
+		int index = songDisplay.getSelectionModel().getSelectedIndex();
+		obsList.remove(index);
+		String songName = updateName.getText();
+		String artistName = updateArtist.getText();
+		String albumName = updateAlbum.getText();
+		int year = Integer.parseInt(updateYear.getText());
+		list.update(index, songName, artistName, albumName, year);
+		obsList.add(songName); //something's up with update function
 	}
 	
 	
