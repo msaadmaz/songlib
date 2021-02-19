b4f4346726c1b8757fce248c705dc088d542ed4b
KEstabillo <kevinmestabillo@gmail.com>
2021-02-18 20:42:49 -0500

    insertion and delete and update error messages

--- src/view/ListController.java
+++ src/view/ListController.java
@@ -3,6 +3,7 @@ package view;
 import java.io.File;
 
 import java.io.FileNotFoundException;
+import java.util.Optional;
 import java.util.Scanner;
 
 import javafx.collections.FXCollections;
@@ -11,6 +12,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.TextInputDialog;
@@ -91,61 +93,111 @@ public class ListController {
 	public void insertion(ActionEvent e) {
 		// need to add alert if name or artist is null
 		// need to add alert if nothing is in insertion text fields ie. all of them are null
-		String songName = Name.getText();
-		String artistName = Artist.getText();
-		String albumName = Album.getText();
-		int year = 0;
-		try {
-			year = Integer.parseInt(Year.getText());
-		} catch( NumberFormatException n ) {
-			if( Year.getText().equals("") ) {
-				year = 0;
-			} else {
-				// need to throw an alert here to say that it has to be numbers inserted not strings
-				System.out.println("Enter in a number");
-			}
-		}
 		
-		obsList = list.add(songName, artistName, albumName, year, obsList);
+		Alert alert = new Alert(AlertType.CONFIRMATION);
+		alert.setTitle("Insert");
+		alert.setContentText("Are you sure you want to insert?");
+		Optional<ButtonType> result = alert.showAndWait();
+		if (result.isPresent() && result.get() == ButtonType.OK) {
+			if(Name.getText().trim().isEmpty() && Artist.getText().trim().isEmpty()) {
+				Alert errorBoth = new Alert(AlertType.ERROR);
+				errorBoth.setContentText("Please input a name and artist");
+				errorBoth.showAndWait();
+			}
+			else if(Name.getText().trim().isEmpty()) {
+				Alert errorName = new Alert(AlertType.ERROR);
+				errorName.setContentText("Please input a name");
+				errorName.showAndWait();
+			}
+			else if(Artist.getText().trim().isEmpty()) {
+				Alert errorAlbum = new Alert(AlertType.ERROR);
+				errorAlbum.setContentText("Please input an album");
+				errorAlbum.showAndWait();
+			}
+			else {
+				
+				try {
+					int year = Integer.parseInt(Year.getText());
+					String songName = Name.getText();
+					String artistName = Artist.getText();
+					String albumName = Album.getText();
+					obsList = list.add(songName, artistName, albumName, year, obsList);
+					songDisplay.getSelectionModel().select(obsList.indexOf(songName));
+					display.setText("Name: " + songName + " \n " +
+							"Artist: " + artistName + " \n  " +
+							"Album: " + albumName + " \n " +
+							"Year: " + year);
+					
+				} catch( NumberFormatException n ) {
+					if( Year.getText().trim().isEmpty() ) {
+						int year = 0;
+						String songName = Name.getText();
+						String artistName = Artist.getText();
+						String albumName = Album.getText();
+						obsList = list.add(songName, artistName, albumName, year, obsList);
+						songDisplay.getSelectionModel().select(obsList.indexOf(songName));
+						display.setText("Name: " + songName + " \n " +
+								"Artist: " + artistName + " \n  " +
+								"Album: " + albumName + " \n " +
+								"Year: " + year);
+					} else {
+						Alert yearError = new Alert(AlertType.ERROR);
+						yearError.setContentText("Please enter in a number");
+						yearError.showAndWait();
+						// need to throw an alert here to say that it has to be numbers inserted not strings
+					}
+				}
+				
+			}
+			
+		 }
 	}
 	
 	public void deletion(ActionEvent e) {
 		//need to add alert if deleting from obslist that has no more songs in it
-		
-		int index = songDisplay.getSelectionModel().getSelectedIndex();
-		obsList.remove(index);
-		list.remove(index);
-		if(obsList.isEmpty()) {
-			display.setText("Name: "  + " \n " +
-					"Artist: "  + " \n  " +
-					"Album: "  + " \n " +
-					"Year: ");
+		Alert alert = new Alert(AlertType.CONFIRMATION);
+		alert.setTitle("Deletion");
+		alert.setContentText("Are you sure you want to delete?");
+		Optional<ButtonType> result = alert.showAndWait();
+		if (result.isPresent() && result.get() == ButtonType.OK) {
+			int index = songDisplay.getSelectionModel().getSelectedIndex();
+			obsList.remove(index);
+			list.remove(index);
+			if(obsList.isEmpty()) {
+				display.setText("Name: "  + " \n " +
+						"Artist: "  + " \n  " +
+						"Album: "  + " \n " +
+						"Year: ");
+			}
 		}
 	}
 	
 	public void update(ActionEvent e) {
 		// need to add alert if update is clicked w/o any entries into the field
-		if(updateName.getText() == null) {
-			System.out.println("name is null");
-		}
-		int index = songDisplay.getSelectionModel().getSelectedIndex();
-		String songName = updateName.getText();
-		String artistName = updateArtist.getText();
-		String albumName = updateAlbum.getText();
-		Integer year = null;
-		try {
-			year = Integer.parseInt(updateYear.getText());
-		} catch( NumberFormatException n ) {
-			if( updateYear.getText().equals("") ) {
-				year = list.list.get(index).year;
-			} else {
-				// need to throw an alert here to say that it has to be numbers inserted not strings
-				System.out.println("Enter in a number");
+		Alert alert = new Alert(AlertType.CONFIRMATION);
+		alert.setTitle("Update");
+		alert.setContentText("Are you sure you want to update?");
+		Optional<ButtonType> result = alert.showAndWait();
+		if (result.isPresent() && result.get() == ButtonType.OK) {
+			int index = songDisplay.getSelectionModel().getSelectedIndex();
+			String songName = updateName.getText();
+			String artistName = updateArtist.getText();
+			String albumName = updateAlbum.getText();
+			Integer year = null;
+			try {
+				year = Integer.parseInt(updateYear.getText());
+			} catch( NumberFormatException n ) {
+				if( updateYear.getText().equals("") ) {
+					year = list.list.get(index).year;
+				} else {
+					// need to throw an alert here to say that it has to be numbers inserted not strings
+					System.out.println("Enter in a number");
+				}
 			}
+			list.update(index, songName, artistName, albumName, year, obsList);
 		}
-		list.update(index, songName, artistName, albumName, year, obsList);
+		
 	}
-	
 	
 	
 }
