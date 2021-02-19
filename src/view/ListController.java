2fb04512cfe00069009d66a14d3e12d72dc9ce45
KEstabillo <kevinmestabillo@gmail.com>
2021-02-19 15:26:04 -0500

    comments

--- src/view/ListController.java
+++ src/view/ListController.java
@@ -135,35 +135,41 @@ public class ListController {
 		alert.setTitle("Insert");
 		alert.setContentText("Are you sure you want to insert?");
 		Optional<ButtonType> result = alert.showAndWait();
-		if (result.isPresent() && result.get() == ButtonType.OK) { 		// confirmation is set to go
-			if(Name.getText().trim().isEmpty() && Artist.getText().trim().isEmpty()) { 		//name and artist are not input, show error
+		// confirmation is set to go
+		if (result.isPresent() && result.get() == ButtonType.OK) { 		
+			//name and artist are not input, show error
+			if(Name.getText().trim().isEmpty() && Artist.getText().trim().isEmpty()) { 		
 				Alert errorBoth = new Alert(AlertType.ERROR);
 				errorBoth.setTitle("Insertion Error");
 				errorBoth.setContentText("Please input a name and artist");
 				errorBoth.showAndWait();
 			}
-			else if(Name.getText().trim().isEmpty()) { 		//name is not input, show error
+			//name is not input, show error
+			else if(Name.getText().trim().isEmpty()) { 		
 				Alert errorName = new Alert(AlertType.ERROR);
 				errorName.setTitle("Insertion Error");
 				errorName.setContentText("Please input a name");
 				errorName.showAndWait();
 			}
-			else if(Artist.getText().trim().isEmpty()) { 		// artist is not input, show error
+			// artist is not input, show error
+			else if(Artist.getText().trim().isEmpty()) { 		
 				Alert errorAlbum = new Alert(AlertType.ERROR);
 				errorAlbum.setTitle("Insertion Error");
 				errorAlbum.setContentText("Please input an album");
 				errorAlbum.showAndWait();
 			}
-			else if(list.search(Name.getText().trim(), Artist.getText().trim())){		//song and artist already exist, show error
+			//song and artist already exist, show error
+			else if(list.search(Name.getText().trim(), Artist.getText().trim())){		
 				Alert errorDuplicate = new Alert(AlertType.ERROR);
 				errorDuplicate.setTitle("Insertion Error");
 				errorDuplicate.setContentText("Song and artist already exist. Please insert the song using other names");
 				errorDuplicate.showAndWait();
 			}
 			else {
-				
-				try {//test for valid year
-					int year = Integer.parseInt(Year.getText()); //will catch if number is not input to year textfield
+				//test for valid year
+				try {
+					//will catch if number is not input to year textfield
+					int year = Integer.parseInt(Year.getText()); 
 					if(year < 0) { //error, list should be positive
 						Alert errorNegativeYear = new Alert(AlertType.ERROR);
 						errorNegativeYear.setTitle("Insertion Error");
@@ -180,13 +186,16 @@ public class ListController {
 								"Album: " + albumName + " \n " +
 								"Year: " + year);
 					}
-				} catch( NumberFormatException n ) {		 // something wrong with year, either not input or wrong values
-					if( Year.getText().trim().isEmpty() ) { 		//no input for year, insert year 0
+					// something wrong with year, either not input or wrong values
+				} catch( NumberFormatException n ) {		 
+					//no input for year, insert year 0
+					if( Year.getText().trim().isEmpty() ) { 		
 						int year = 0;
 						String songName = Name.getText().trim();
 						String artistName = Artist.getText().trim();
 						String albumName;
-						if(Album.getText().trim().isEmpty()) { // no album present
+						// no album present
+						if(Album.getText().trim().isEmpty()) { 
 							albumName = "No Album";
 						}else {
 							albumName = Album.getText().trim();
@@ -197,7 +206,9 @@ public class ListController {
 								"Artist: " + artistName + " \n  " +
 								"Album: " + albumName + " \n " +
 								"Year: " + year);
-					} else { // year is not a number
+					} 
+					// year is not a number
+					else { 
 						Alert yearError = new Alert(AlertType.ERROR);
 						yearError.setContentText("Please enter in a valid year");
 						yearError.showAndWait();
@@ -215,15 +226,18 @@ public class ListController {
 		alert.setTitle("Deletion Confirmation");
 		alert.setContentText("Are you sure you want to delete?");
 		Optional<ButtonType> result = alert.showAndWait();
-		if (result.isPresent() && result.get() == ButtonType.OK) { 	// Confirmation of Deletion
-			if(obsList.isEmpty()) {		//if list is empty, do nothing
+		// Confirmation of Deletion
+		if (result.isPresent() && result.get() == ButtonType.OK) { 	
+			//if list is empty, do nothing
+			if(obsList.isEmpty()) {		
 				Alert errorEmpty = new Alert(AlertType.ERROR);
 				errorEmpty.setTitle("Deletion Error");
 				errorEmpty.setContentText("There is nothing to delete.");
 				errorEmpty.show();
 			} else {
 				int index = songDisplay.getSelectionModel().getSelectedIndex();
-				if(index == -1) {	//select first song
+				//select first song
+				if(index == -1) {	
 					index = 0;
 				}
 				obsList.remove(index);
@@ -233,7 +247,8 @@ public class ListController {
 							"Artist: "  + " \n  " +
 							"Album: "  + " \n " +
 							"Year: ");
-				} else if (index > obsList.size()-1 && index >= 1) {	//Deletion will delete last item in song list
+					//Deletion will delete last item in song list
+				} else if (index > obsList.size()-1 && index >= 1) {	
 					index--;
 					songDisplay.getSelectionModel().select(index);
 					Song selectedSong = list.list.get(index);
@@ -242,7 +257,8 @@ public class ListController {
                                     "Album: " + selectedSong.album + " \n " +
                                     "Year: " + selectedSong.year);
 					
-				} else {	//Delete Selected item
+				}//Delete Selected item 
+				else {	
 					songDisplay.getSelectionModel().select(index);
 					Song selectedSong = list.list.get(index);
                     display.setText("Name: " + selectedSong.name + " \n " +
@@ -253,38 +269,45 @@ public class ListController {
 			}
 		}
 	}
-	
-	public void update(ActionEvent e) {		//Update button is clicked
+	//Update button is clicked
+	public void update(ActionEvent e) {		
 		Alert alert = new Alert(AlertType.CONFIRMATION);
 		alert.setTitle("Update");
 		alert.setContentText("Are you sure you want to update?");
 		Optional<ButtonType> result = alert.showAndWait();
-		if (result.isPresent() && result.get() == ButtonType.OK) {		//Confirmation is ok				
-			if(list.search(updateName.getText().trim(), updateArtist.getText().trim())){ //searches for song and artist already in list
+		//Confirmation is ok	
+		if (result.isPresent() && result.get() == ButtonType.OK) {					
+			//searches for song and artist already in list
+			if(list.search(updateName.getText().trim(), updateArtist.getText().trim())){ 
 				Alert errorDuplicate = new Alert(AlertType.ERROR);
 				errorDuplicate.setTitle("Update Error");
 				errorDuplicate.setContentText("Song and artist already exist. Please update the song using other names");
 				errorDuplicate.showAndWait();
 			} else{
-				try {//checks for valid year
+				//checks for valid year
+				try {
 					int year = Integer.parseInt(updateYear.getText().trim());
-					if(year < 0) { //checks for positive year
+					//checks for positive year
+					if(year < 0) { 
 						Alert errorNegativeYear = new Alert(AlertType.ERROR);
 						errorNegativeYear.setTitle("Update Error");
 						errorNegativeYear.setContentText("Please insert a valid year that you wish to change the currently selected song to.");
 						errorNegativeYear.showAndWait();
-					}else { //year is valid
+					}//year is valid
+					else { 
 						int index = songDisplay.getSelectionModel().getSelectedIndex();
 						String songName = updateName.getText().trim();
 						String artistName = updateArtist.getText().trim();
 						if( !songName.equals("") || !artistName.equals("") ) {
-							if(!songName.equals("")) { //SongName is updated
-								if( list.search(songName, list.list.get(index).artist)) { //Updating song will produce a duplicate 
+							//SongName is updated
+							if(!songName.equals("")) { 
+								//Updating song will produce a duplicate 
+								if( list.search(songName, list.list.get(index).artist)) { 
 									Alert errorDuplicate = new Alert(AlertType.ERROR);
 									errorDuplicate.setTitle("Update Error");
 									errorDuplicate.setContentText("Song and artist already exist. Please update the song using other names");
 									errorDuplicate.showAndWait();
-								} else { // no duplicate is created, so updates the song
+								} else { // no duplicate is created, so updates the song 
 									String albumName = updateAlbum.getText().trim();
 									list.update(index, songName, artistName, albumName, year, obsList);
 									Song selectedSong = list.list.get(index);
